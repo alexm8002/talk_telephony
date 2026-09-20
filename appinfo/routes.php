9a6f400adae51c4dc1892a00d4d4cf03d6ca8678
alexm8002 <a.m.a.martinez@icloud.com>
2026-09-20 19:02:38 +0200

    Add SPDX license comments to routes.php
    Added SPDX license information to routes.php file.

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * SPDX-FileCopyrightText: 2026 2M Production Electrique
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 return [
     'routes' => [
         ['name' => 'account#getPersonal', 'url' => '/api/v1/account', 'verb' => 'GET'],
